d9563b7680d70379bfc3b35b2bfa4a14f6e9eb1b
hilmi.albiruni <hilmi.albiruni@ui.ac.id>
2023-01-03 16:19:41 +0700

    login

--- belajarbelajar/src/main/java/apap/tutorial/belajarbelajar/rest/Setting.java
+++
@@ -1,6 +0,0 @@
-package apap.tutorial.belajarbelajar.rest;
-
-public class Setting {
-    final public static String courseUrl = "https://77431479-0b90-454a-9394-bbc8c1cc4110.mock.pstmn.io";
-    final public static String genderizeUrl = "https://api.genderize.io";
-}
--- mygalaxy/src/main/java/myapp/controller/HomeController.java
+++ mygalaxy/src/main/java/myapp/controller/HomeController.java
@@ -2,11 +2,17 @@ package myapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    private String Home() {
-        return "index";
+    @RequestMapping("/")
+    private String home() {
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
     }
 }
